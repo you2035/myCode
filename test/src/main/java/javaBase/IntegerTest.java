3b660a5cb85d1b63402cdcf191ce881741c06a5a
luxinxin <1649096790@qq.com>
2019-07-01 22:23:01 +0800

    接口和抽象类

--- test/src/main/java/javaBase/IntegerTest.java
+++ test/src/main/java/javaBase/IntegerTest.java
@@ -19,6 +19,11 @@ public class IntegerTest {
         Integer n = 12334;
         System.out.println(m == n);
 
+//        float f= 1.1;
+        float f = 1.1f;
+        short s1 = 1;
+        s1 = (short) (s1 + 1);
+        s1 += 1;
 
     }
 }
