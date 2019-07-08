253297a0fea91a1542e32ad801fac34573b27b13
luxinxin <1649096790@qq.com>
2019-07-08 22:12:36 +0800

    多线程

---
+++ test/src/main/java/javaBase/collection/CollectionTest.java
@@ -0,0 +1,9 @@
+package javaBase.collection;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CollectionTest {
+    public static void main(String[] args) {
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+    }
+}
---
+++ test/src/main/java/multithreading/SynchronizedExample.java
@@ -0,0 +1,23 @@
+package multithreading;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SynchronizedExample {
+
+    public static void main(String[] args) {
+        SynchronizedExample e1 = new SynchronizedExample();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(() -> e1.func1());
+        executorService.execute(() -> e1.func1());
+    }
+
+    public void func1() {
+        synchronized (this) {
+            for (int i = 0; i < 10; i++) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+}
