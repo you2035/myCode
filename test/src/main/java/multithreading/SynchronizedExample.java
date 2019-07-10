00d1a0f21c55a83101d4791dd0b589363eebb476
luxinxin <1649096790@qq.com>
2019-07-10 21:42:41 +0800

    多线程

--- test/src/main/java/multithreading/SynchronizedExample.java
+++ test/src/main/java/multithreading/SynchronizedExample.java
@@ -7,13 +7,22 @@ public class SynchronizedExample {
 
     public static void main(String[] args) {
         SynchronizedExample e1 = new SynchronizedExample();
+        SynchronizedExample e2 = new SynchronizedExample();
         ExecutorService executorService = Executors.newCachedThreadPool();
-        executorService.execute(() -> e1.func1());
-        executorService.execute(() -> e1.func1());
+        executorService.execute(() -> e1.func2());
+        executorService.execute(() -> e2.func2());
     }
 
     public void func1() {
         synchronized (this) {
+            for (int i = 0; i < 10; i++) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public void func2() {
+        synchronized (SynchronizedExample.class) {
             for (int i = 0; i < 10; i++) {
                 System.out.print(i + " ");
             }
