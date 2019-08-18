c5cce513c2b576acfdc7c10d1441e7036ede1736
liuxin <1649096790@qq.com>
2019-08-18 22:08:14 +0800

    多线程

---
+++ test/src/main/java/javaBase/IO/FileOperate.java
@@ -0,0 +1,47 @@
+package javaBase.IO;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOperate {
+    public static void main(String[] args) {
+        File file = new File("D:\\Downloads\\pic");
+        listAllFiles(file);
+    }
+
+    public static void listAllFiles(File dir){
+        if(dir == null || !dir.exists()){
+            return;
+        }
+        if(dir.isFile()){
+            System.out.println(dir.getName());
+            return;
+        }
+
+        if(dir.isDirectory()){
+            for (File file  :dir.listFiles() ) {
+                listAllFiles(file);
+            }
+        }
+    }
+
+    public static void copyFile(String src, String dist) throws IOException {
+        FileInputStream in = new FileInputStream(src);
+        FileOutputStream out = new FileOutputStream(dist);
+
+        byte[] buffer = new byte[20 * 1024];
+        int cnt;
+
+        // read() 最多读取 buffer.length 个字节
+        // 返回的是实际读取的个数
+        // 返回 -1 的时候表示读到 eof，即文件尾
+        while ((cnt = in.read(buffer, 0, buffer.length)) != -1) {
+            out.write(buffer, 0, cnt);
+        }
+
+        in.close();
+        out.close();
+    }
+}
